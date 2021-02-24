e316011e3b1bbf7b63f7f863c2fdc400c5c3b965
duran <1010506985@qq.com>
2021-02-25 00:36:18 +0800

    添加springCloud 注册中心

---
+++ eureka-server-some-two/src/main/java/com/study/eurekaserversometwo/EurekaServerSomeTwoApplication.java
@@ -0,0 +1,21 @@
+package com.study.eurekaserversometwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * 注册中心 高可用版
+ * eureka-server-some-one
+ * eureka-server-some-two
+ * 服务启动后 访问ip地址
+ * http://127.0.0.1:7002/
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerSomeTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerSomeTwoApplication.class, args);
+    }
+
+}
